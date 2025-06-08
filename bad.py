da2afd3833f77df87887befd804b048548b9c9ad
peter parker <tanishqbokade555@gmail.com>
2025-06-08 08:59:56 +0530

    Update bad.py

--- bad.py
+++ bad.py
@@ -6,4 +6,4 @@ from pyrogram import filters
 
 load_dotenv()
 
-SUKH = getenv("SUKH", "mongodb+srv://Badmunda_13:badmunda50@cluster0.9oyzqux.mongodb.net/?retryWrites=true&w=majority")
+SUKH = getenv("SUKH", "mongodb+srv://tanishbokade555:<Tanishq@12345>@cluster0.b9hlgkz.mongodb.net/?retryWrites=true&w=majority")
